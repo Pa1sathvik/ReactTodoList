a2cec20e9155e146bd3a25ee833cdcdb5e237b75
vsathvik <7vik.sathvik@gmail.com>
2020-07-28 15:52:19 +0530

    Added reset value...

--- src/StateChange.js
+++ src/StateChange.js
@@ -1,28 +1,37 @@
-import React, { Component } from "react"
-
+import React, { Component } from "react";
 
 export default class StageChange extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: this.props.value,
+    };
+  }
 
-    constructor(props){
+  render() {
+    return (
+      <div>
+       
 
-        super(props);
-        this.state = {
-
-            value : this.props.value,
-
-
-        }
-    }
-
-    render(){
-
-        return(
-
-            <div onClick = {()=>{this.setState({
-
-                value : this.state.value + 190,
-            })}}>Value is {this.state.value}</div>
-        )
-    }
-
+        <br />
+        <div
+          onClick={() => {
+            this.setState({
+              value: 0,
+            });
+          }}
+        > Reset Value with click
+        </div>
+        <div
+        onClick={() => {
+          this.setState({
+            value: this.state.value + 190,
+          });
+        }}
+      >
+        Value is {this.state.value}
+      </div>
+      </div>
+    );
+  }
 }
